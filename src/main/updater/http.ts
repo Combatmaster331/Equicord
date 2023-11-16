bf9733fb2360d2f3dc907bca3822ec20b15cc2a0
thororen <78185467+thororen1234@users.noreply.github.com>
2023-11-16 17:44:08 -0500

    Remove Extra Debug Stuff

--- src/main/updater/http.ts
+++ src/main/updater/http.ts
@@ -52,7 +52,6 @@ async function calculateGitChanges() {
     const res = await githubGet(`/compare/${gitHash}...HEAD`);
 
     //const data = JSON.parse(res.data.toString("utf-8"));
-    console.info('calculateGitChanges:', res.data);
     return res.data.commits.map((c: any) => ({
         // github api only sends the long sha
         hash: c.sha.slice(0, 7),
@@ -80,7 +79,6 @@ async function fetchUpdates() {
 async function applyUpdates() {
     await Promise.all(PendingUpdates.map(
         async ([name, data]) => {
-            console.info({ function: 'applyUpdates', name: name, url: data });
             writeFile(
                 join(__dirname, name),
                 (await axios({
