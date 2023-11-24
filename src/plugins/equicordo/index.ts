cdd987149e988dadcf719add609eadb064186e15
thororen <78185467+thororen1234@users.noreply.github.com>
2023-11-24 11:07:22 -0500

    Fix Equicordo

--- src/plugins/equicordo/index.ts
+++ src/plugins/equicordo/index.ts
@@ -7,10 +7,10 @@ export default definePlugin({
     authors: [Devs.echo, Devs.thororen],
     patches: [
         {
-            find: "ae7d16bb2eea76b9b9977db0fad66658.mp3",
+            find: "44208515805198d4c548.mp3",
             replacement: {
-                match: /e\.exports=n\.p+"ae7d16bb2eea76b9b9977db0fad66658.mp3";/,
-                replace: 'e.exports="https://github.com/Equicord/Ignore/raw/main/equicordo.mp3";'
+                match: /e\.exports=n\.p\+"[a-zA-Z0-9]+\.mp3"/,
+                replace: 'e.exports="https://github.com/Equicord/Ignore/raw/main/equicordo.mp3"'
             }
         },
     ],
