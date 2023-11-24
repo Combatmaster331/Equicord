07b9a4af12cf3119f9ffdaaf15b95149abcfd9c4
thororen <78185467+thororen1234@users.noreply.github.com>
2023-11-24 09:18:32 -0500

    Update index.ts

--- src/plugins/equicordo/index.ts
+++ src/plugins/equicordo/index.ts
@@ -6,9 +6,9 @@ export default definePlugin({
     description: "Replaces the discordo sound on startup with something a bit more interesting",
     authors: [Devs.echo],
     patches: [{
-        find: "ae7d16bb2eea76b9b9977db0fad66658.mp3",
+        find: "ae7d16bb2eea76b9b9977db0fad66658",
         replacement: {
-            match: /e\.exports=n\.p\+"[a-zA-Z0-9]+\.mp3"/,
+            match: 'e.exports=ae7d16bb2eea76b9b9977db0fad66658.mp3',
             replace: 'e.exports="https://github.com/Equicord/Ignore/raw/main/equicordo.mp3";'
         }
     }]
