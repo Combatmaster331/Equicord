056a157a4e2f18dd642a1d2d6e6d4059f50d84b9
thororen <78185467+thororen1234@users.noreply.github.com>
2023-11-24 10:40:06 -0500

    Fix spacing

--- src/plugins/equicordo/index.ts
+++ src/plugins/equicordo/index.ts
@@ -5,11 +5,13 @@ export default definePlugin({
     name: "Equicordo",
     description: "Replaces the discordo sound on startup with something a bit more interesting",
     authors: [Devs.echo, Devs.thororen],
-    patches: [{
-        find: "ae7d16bb2eea76b9b9977db0fad66658.mp3",
-        replacement: {
-            match: /e\.exports=n\.p+"ae7d16bb2eea76b9b9977db0fad66658.mp3";/,
-            replace: 'e.exports="https://github.com/Equicord/Ignore/raw/main/equicordo.mp3";'
-        }
-    }]
+    patches: [
+        {
+            find: "ae7d16bb2eea76b9b9977db0fad66658.mp3",
+            replacement: {
+                match: /e\.exports=n\.p+"ae7d16bb2eea76b9b9977db0fad66658.mp3";/,
+                replace: 'e.exports="https://github.com/Equicord/Ignore/raw/main/equicordo.mp3";'
+            }
+        },
+    ],
 });
