d5328899d40a796f16de7df568cde55e88c8dd73
FoxStorm1 <69159547+Yeetov@users.noreply.github.com>
2023-11-25 12:22:43 +0000

    Updated description in equicordo

--- src/plugins/equicordo/index.ts
+++ src/plugins/equicordo/index.ts
@@ -3,8 +3,8 @@ import definePlugin from "@utils/types";;
 
 export default definePlugin({
     name: "Equicordo",
-    description: "Replaces the discordo sound on startup with something a bit more interesting",
-    authors: [Devs.echo, Devs.thororen],
+    description: "Replaces the discordo sound on startup with something a bit more interesting. Sound by FoxStorm1.",
+    authors: [Devs.echo, Devs.thororen], [Devs.FoxStorm1],
     patches: [
         {
             find: "44208515805198d4c548.mp3",
