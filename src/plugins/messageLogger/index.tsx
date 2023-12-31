7c3519e883bfad05bb831d51827925c7c8014792
thororen <78185467+thororen1234@users.noreply.github.com>
2023-12-30 19:47:47 -0500

    Updates

--- src/plugins/messageLogger/index.tsx
+++ src/plugins/messageLogger/index.tsx
@@ -327,6 +327,7 @@ export default definePlugin({
             // Attachment renderer
             // Module 96063
             find: ".removeAttachmentHoverButton",
+            group: true,
             replacement: [
                 {
                     match: /(className:\i,attachment:\i),/,
