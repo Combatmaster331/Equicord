985e3ce71d021d7be581a4ad19b95b9d1bbd177e
thororen <78185467+thororen1234@users.noreply.github.com>
2024-01-16 11:08:12 -0500

    Add back replyTimestamp

---
+++ src/plugins/replyTimestamp/index.tsx
@@ -0,0 +1,51 @@
+import "./style.css";
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+import { findByPropsLazy } from "@webpack";
+import { Timestamp } from "@webpack/common";
+
+const MessageIds = findByPropsLazy("getMessageTimestampId");
+const DateUtils = findByPropsLazy("calendarFormat", "dateFormat", "isSameDay", "accessibilityLabelCalendarFormat");
+const MessageClasses = findByPropsLazy("separator", "latin24CompactTimeStamp");
+
+function Sep(props) {
+    return <i className={MessageClasses.separator} aria-hidden={true} {...props} />;
+}
+
+export default definePlugin({
+    name: "ReplyTimestamp",
+    description: "Shows a timestamp on replied-message previews",
+    authors: [Devs.Kyuu],
+
+    patches: [
+        {
+            find: ",{renderSingleLineMessage:function(){return ",
+            replacement: {
+                match: /(?<="aria-label":\w+,children:\[)(?=\w+,\w+,\w+\])/,
+                replace: "$self.ReplyTimestamp(arguments[0]),"
+            }
+        }
+    ],
+
+    ReplyTimestamp({ referencedMessage, baseMessage }) {
+        if (referencedMessage.state === 0) {
+            const refTimestamp = referencedMessage.message.timestamp;
+            const baseTimestamp = baseMessage.timestamp;
+            return <Timestamp
+                id={MessageIds.getMessageTimestampId(referencedMessage.message)}
+                className="c98-reply-timestamp"
+                compact={refTimestamp.isSame(baseTimestamp, "date")}
+                timestamp={refTimestamp}
+                isInline={false}
+            >
+                <Sep>[</Sep>
+                {DateUtils.isSameDay(refTimestamp, baseTimestamp)
+                    ? DateUtils.dateFormat(refTimestamp, "LT")
+                    : DateUtils.calendarFormat(refTimestamp)
+                }
+                <Sep>]</Sep>
+            </Timestamp>;
+        }
+    },
+});
