bbe427a7de4b47ba7e33d8879132eef749db0ff7
thororen <78185467+thororen1234@users.noreply.github.com>
2024-03-24 17:20:55 -0400

    Testing

--- src/plugins/unlimitedAccounts/index.ts
+++ src/plugins/unlimitedAccounts/index.ts
@@ -27,8 +27,8 @@ export default definePlugin({
         {
             find: "multiaccount_cta_tooltip_seen",
             replacement: {
-                match: /(let \i=)\d+(,\i="switch-accounts-modal",\i="multiaccount_cta_tooltip_seen")/,
-                replace: '$1Infinity$2',
+                match: /(let \i=)/,
+                replace: "$1Infinity",
             },
         },
     ],
