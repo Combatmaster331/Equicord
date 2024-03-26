1c58db60143c119c3b268cfbc3fff235b52381eb
thororen <78185467+thororen1234@users.noreply.github.com>
2024-03-26 17:13:30 -0400

    GlobalBadges in ShowBadgesInChat

--- src/plugins/showBadgesInChat/index.tsx
+++ src/plugins/showBadgesInChat/index.tsx
@@ -10,8 +10,9 @@ import { Devs } from "@utils/constants";
 import { isPluginDev } from "@utils/misc";
 import definePlugin, { OptionType } from "@utils/types";
 import { findByProps } from "@webpack";
-
 import BadgeApi from "../../plugins/_api/badges";
+import { React } from "@webpack/common";
+import { CustomBadge, BadgeCache } from "./types";
 
 let RoleIconComponent: React.ComponentType<any> = () => null;
 let roleIconClassName = "";
@@ -30,6 +31,51 @@ const discordBadges: readonly [number, string, string][] = Object.freeze([
     [9, "Early Supporter", "7060786766c9c840eb3019e725d2b358"],
     [18, "Moderator Programs Alumni", "fee1624003e2fee35cb398e125dc479b"]
 ]);
+
+const API_URL = "https://clientmodbadges-api.herokuapp.com/";
+const cache = new Map<string, BadgeCache>();
+const EXPIRES = 1000 * 60 * 15;
+
+const fetchBadges = (id: string): BadgeCache["badges"] | undefined => {
+    const cachedValue = cache.get(id);
+    if (!cache.has(id) || (cachedValue && cachedValue.expires < Date.now())) {
+        fetch(`${API_URL}users/${id}`)
+            .then(res => res.json() as Promise<BadgeCache["badges"]>)
+            .then(body => {
+                cache.set(id, { badges: body, expires: Date.now() + EXPIRES });
+                return body;
+            });
+    } else if (cachedValue) {
+        return cachedValue.badges;
+    }
+};
+
+const GlobalBadges = (userID: string) => {
+    const [badges, setBadges] = React.useState<BadgeCache["badges"]>({});
+    React.useEffect(() => setBadges(fetchBadges(userID) ?? {}), [userID]);
+    const globalBadges: JSX.Element[] = [];
+
+    Object.keys(badges).forEach(mod => {
+        if (mod.toLowerCase() === "vencord") return;
+        badges[mod].forEach(badge => {
+            if (typeof badge === "string") {
+                badge = {
+                    name: badge,
+                    badge: `${API_URL}badges/${mod}/${(badge as string).replace(mod, "").trim().split(" ")[0]}`
+                };
+            } else if (typeof badge === "object") badge.custom = true;
+            const cleanName = badge.name.replace(mod, "").trim();
+            if (!badge.custom) badge.name = `${cleanName.charAt(0).toUpperCase() + cleanName.slice(1)}`;
+            globalBadges.push(<RoleIconComponent className={roleIconClassName} name={badge.name} size={20} src={badge.badge} />);
+        });
+    });
+
+    return [
+        <span style={{ order: settings.store.globalBadgesPosition }}>
+            {globalBadges}
+        </span>
+    ];
+};
 
 function vencordDonorChatBadges(userID: string) {
     return [
@@ -95,6 +141,7 @@ function discordNitroChatBadge(userPremiumType: number) {
 
 function ChatBadges({ author }) {
     const chatBadges = [
+        ...settings.store.showGlobalBadges ? GlobalBadges(author.id) : [],
         ...settings.store.showVencordDonorBadges ? vencordDonorChatBadges(author.id) : [],
         ...settings.store.showVencordEquicordContributorBadges ? vencordEquicordContributorChatBadge(author.id) : [],
         ...settings.store.showDiscordProfileBadges ? discordProfileChatBadges(author.flags || author.publicFlags) : [],
@@ -129,14 +176,14 @@ const settings = definePluginSettings({
         description: "The position of the Vencord/Equicord Contributor badge.",
         default: 1
     },
-    showDiscordProfileBadges: {
+    showGlobalBadges: {
         type: OptionType.BOOLEAN,
-        description: "Enable to show Discord profile badges in chat.",
+        description: "Enable to show Global Badges in chat. A bit buggy so switch channels for it to load.",
         default: true
     },
-    discordProfileBadgesPosition: {
+    globalBadgesPosition: {
         type: OptionType.NUMBER,
-        description: "The position of the Discord profile badges.",
+        description: "The position of the Discord Nitro badge.",
         default: 2
     },
     showDiscordNitroBadges: {
@@ -144,17 +191,27 @@ const settings = definePluginSettings({
         description: "Enable to show Discord Nitro badges in chat.",
         default: true
     },
+    discordProfileBadgesPosition: {
+        type: OptionType.NUMBER,
+        description: "The position of the Discord profile badges.",
+        default: 3
+    },
+    showDiscordProfileBadges: {
+        type: OptionType.BOOLEAN,
+        description: "Enable to show Discord Nitro badges in chat.",
+        default: true
+    },
     discordNitroBadgePosition: {
         type: OptionType.NUMBER,
         description: "The position of the Discord Nitro badge.",
-        default: 3
+        default: 4
     }
 });
 
 export default definePlugin({
     name: "ShowBadgesInChat",
     authors: [Devs.Shalev, Devs.fres, Devs.ryan, Devs.KrystalSkull],
-    description: "Shows profile badges in chat. Shoutout krystalskullofficial for the idea.",
+    description: "Shows profile badges in chat. That includes built in Discord Badges. Also shows Vencord Contributor Badges and all Donor badges.",
     dependencies: ["MessageDecorationsAPI"],
     patches: [
         {
---
+++ src/plugins/showBadgesInChat/types.d.ts
@@ -0,0 +1,10 @@
+export type CustomBadge = string | {
+    name: string;
+    badge: string;
+    custom?: boolean;
+};
+
+export interface BadgeCache {
+    badges: { [mod: string]: CustomBadge[]; };
+    expires: number;
+}
